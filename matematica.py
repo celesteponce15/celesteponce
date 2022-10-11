6816110f93082bef4746981a0bdbf8176dd31720
celeste <celestepon552@gmail.com>
2022-10-11 19:18:17 -0300

    inicio de proyecto

---
+++ matematica.py
@@ -0,0 +1,23 @@
+from unittest import result
+from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
+
+def sumar (a,b):
+    Resultado=a+b
+    return Resultado
+print(sumar(5,6))
+
+def resta (a,b):
+    Resultado=a-b
+    return Resultado
+print(resta(5,6))
+
+def dividir (a,b):
+    Resultado=a/b
+    return Resultado
+    
+print(dividir(5,6))
+
+def multiplicacion (a,b):
+    Resultado=a*b
+    multiplicacion Resultado
+print(multiplicacion(5,6))
